ac30bd8fab94cfbe696eed314862b9e23bea9869
Christa <christatingzon@gmail.com>
2022-04-19 23:22:51 +0800

    fetch tasks

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,7 +1,14 @@
 <template>
-  <Tutorial />
 </template>
 
 <script>
-export default {}
+export default {
+  // All data from Strapi should be queried from the asyncData function
+  async asyncData ({ $strapi }) {
+    const [tasks] = await Promise.all([$strapi.find('tasks')])
+    return {
+      tasks
+    }
+  }
+}
 </script>
