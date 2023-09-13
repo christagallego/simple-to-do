4162891b2ca0298dd3453a300f6f1488e2d9303a
Christa <gallegochrista@gmail.com>
2023-09-13 16:11:55 +0800

    make index tasks fetching work

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -3,7 +3,11 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/strapi'],
   strapi: {
-    url: 'http://localhost:1337'
+    url: process.env.STRAPI_URL || 'http://localhost:1337',
+    prefix: '/api',
+    version: 'v4',
+    cookie: {},
+    cookieName: 'strapi_jwt'
   },
   css: ['~/assets/css/main.css'],
   postcss: {
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,13 +1,16 @@
 <template>
-  <h1 class="text-2xl font-bold underline">Hello {{ name }}!</h1>
+  <h1 class="text-2xl font-bold underline">Hello! {{ tasks }}</h1>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      name: 'Rambo'
-    }
+<script setup lang="ts">
+import type { Task } from '~/types'
+const { find } = useStrapi()
+const { data: dataRes } = await useAsyncData(
+  'tasks',
+  () => find<Task>('tasks'),
+  {
+    server: false
   }
-}
+)
+const tasks = dataRes?.value?.data
 </script>
